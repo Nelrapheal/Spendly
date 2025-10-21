3151c5e8640a13e23bfc3e8bce7b5478e7ee1193
Nelrapheal <nelrapholafisoye20@gmail.com>
2025-10-21 12:10:41 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,50 @@
+const CACHE_NAME = "spendly-cache-v2"; // change version anytime you update
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./home.html",
+  "./home.css",
+  "./home.js",
+  "./manifest.json"
+];
+
+// Install and cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("spendly-cache").then((cache) => {
-      return cache.addAll([
-        "./index.html",
-        "./home.css",
-        "./home.js",
-        "./manifest.json",
-        "./favicon.ico",
-        "./favicon-16x16.png",
-        "./favicon-32x32.png",
-        "./android-chrome-192x192.png",
-        "./android-chrome-512x512.png"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting(); // activate immediately
 });
 
+// Activate and remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim(); // take control immediately
+});
+
+// Fetch from cache first, then network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).then((res) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, res.clone());
+            return res;
+          });
+        })
+      );
     })
   );
 });
